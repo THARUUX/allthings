4bfee1b34a8d31393273c1dc99a8748fe8f19f9a
THARUUX <tdamsara6@gmail.com>
2026-06-02 22:50:14 +0530

    first commit

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,9 +1,9 @@
 import { MetadataRoute } from "next";
 
+
 export default function robots(): MetadataRoute.Robots {
   // Use safe helper to ensure protocol
-import { getBaseUrl } from '@/lib/url';
-const baseUrl = getBaseUrl().toString();
+    const baseUrl = getBaseUrl().toString();
 
   return {
     rules: [
